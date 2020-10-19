742ea9c450d8e9eeb3980be1a718275558215230
n34064730 <inandeniz.bal@gmail.com>
2020-10-19 17:09:59 +0000

    silinen 6. soruyu tekrardan yüklüyorum

---
+++ lab2/6.py
@@ -0,0 +1,8 @@
+#tekrardan hız sorusu, sanırım değişkenleri atamadığımdan silindi idk
+vcar1 = 80
+vcar2 = 70
+fdistance = 490
+wdistance = 150
+hdistance = fdistance - wdistance
+minut = (hdistance / (vcar1 + vcar2)) / 60
+print("Required time is : ",minut)
