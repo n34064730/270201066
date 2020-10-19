32cf97ee4a43a2e4db370c4857718f97115ee4c8
n34064730 <inandeniz.bal@gmail.com>
2020-10-19 17:03:36 +0000

    Labda yaptığım son 2 soru silinmiş gözüküyor bu sebepten tekrar yüklüyorum

---
+++ lab2/5.py
@@ -0,0 +1,4 @@
+kenar1= int(input("Enter the first edge of triangle: "))
+kenar2= int(input("Enter the second edge of triangle: "))
+hip = (kenar1**2 + kenar2**2)**(1/2)
+print("Hypetenose of {} {} triangle is {}".format(kenar1,kenar2,hip))
