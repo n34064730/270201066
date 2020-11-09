ca07ad9083fd955fce8f5b879bc7478d9bed3f35
n34064730 <inandeniz.bal@gmail.com>
2020-11-09 09:07:11 +0000

    2. soru (1'de hala eksik yerlerim var)

--- lab4/1.py
+++ lab4/1.py
@@ -1,10 +1,16 @@
 ilk = int(input("Enter the first number: "))
 ikinci = int(input("Enter the second number: "))
-toplam = ilk + ikinci
-if (toplam >= 100):
-  print(toplam - 100)
-elif(10<=toplam<100):
-  print(toplam)
-elif(0<=toplam<10):
+toplam =str( ilk + ikinci)
+basamak= len(toplam)
+
+liste1= []
+if (basamak <2):
   print("0",toplam)
+elif (basamak>=2):
+  for i in toplam:
+    liste1.append(i)
+  for i in liste1:
+      
+  
+
 
