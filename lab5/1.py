48530c1aab9c841cba2ccea70ac777116d525965
n34064730 <inandeniz.bal@gmail.com>
2020-11-16 09:16:06 +0000

    2. soru

--- lab5/1.py
+++ lab5/1.py
@@ -3,5 +3,5 @@ sonuc=1
 sayı = int(input("Enter the number"))
 while çarpan <=10:
   sonuc= sayı*çarpan
-  print(sonuc)
+  print("{} x {} = {}".format(sayı,çarpan,sonuc))
   çarpan +=1
---
+++ lab5/2.py
@@ -0,0 +1,9 @@
+adet = int(input("How many numbers: "))
+cift = 0
+number = 0
+for i in range(adet):
+  number= int(input("Enter number "))
+  if number %2 ==0:
+    cift +=1
+sonuc = (cift/ adet)*100
+print("%",sonuc)    
