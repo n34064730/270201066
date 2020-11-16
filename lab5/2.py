15702949ececea0638191d605759d96c99953ff7
n34064730 <inandeniz.bal@gmail.com>
2020-11-16 09:17:55 +0000

    nothing

--- lab5/2.py
+++ lab5/2.py
@@ -3,7 +3,10 @@ cift = 0
 number = 0
 for i in range(adet):
   number= int(input("Enter number "))
-  if number %2 ==0:
+  if (number <0):
+    print("Sadece pozitif sayıları girebilirsiniz !!!")
+    
+  elif number %2 ==0:
     cift +=1
 sonuc = (cift/ adet)*100
 print("%",sonuc)    
