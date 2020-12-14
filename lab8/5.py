51e96c4f24fb859953abed35e5aa6b7b080f2023
n34064730 <inandeniz.bal@gmail.com>
2020-12-14 14:37:16 +0000

    test

--- lab8/5.py
+++ lab8/5.py
@@ -13,11 +13,17 @@ def password_checker(password):
       elif i.isdigit():
         xsayac+=1
       elif i.isalpha():
-        ysayac+=1:
-      elif i.is.printable():
+        ysayac+=1
+      elif i.isprintable():
         zsayac+=1
-  if(sapace=!0):
+  if(space!=0):
     return("{}\t(Level 0)".format(password))
-  elif((xsayac!=0)or(ysayac!=0)or(zsayac!=0)):
-    
+  elif((xsayac!=0)and(ysayac!=0)and(zsayac!=0)):
+    return("{}\t(Level 3)".format(password))
+  elif((xsayac!=0 and ysayac!=0 and zsayac==0)or(xsayac!=0 and zsayac!=0 and ysayac==0)or(ysayac!=0 and zsayac!=0 and xsayac==0)):
+    return("{}\t(Level 2)".format(password))
+  elif((xsayac !=0 and ysayac==0 and zsayac==0)or(xsayac==0 and ysayac!=0 and zsayac==0)or(xsayac==0 and ysayac==0 and zsayac!=0)):
+    return("{}\t(Level 1)".format(password))
+password=input("Enter password:")
+print(password_checker(password))    
 
