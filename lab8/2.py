617bfe9496e4731ae5cfa791f8b4d9ad290a50c7
n34064730 <inandeniz.bal@gmail.com>
2020-12-14 09:02:14 +0000

    2.soru

--- lab8/2.py
+++ lab8/2.py
@@ -1,4 +1,4 @@
-def prime(number):
+def is_prime(number):
   if number==2:
     return True
   else:
@@ -9,7 +9,24 @@ def prime(number):
       else:
         return True
 number=int(input("Enter number: "))
-if prime(number):
+if is_prime(number):
   print("{} is a prime number".format(number))
 else:
   print("{} is not a prime number".format(number))
+def print_primes_between(number1,number2):
+  liste=[]
+  for i in range(number1+1,number2):
+    if i==2:
+      liste.append(i)
+    else:
+      for x in range(i):
+        if i%x!=0:
+          liste.append(i)
+  return liste
+number1=int(input("Enter the first number:"))
+number2=int(input("Enter the second number:"))
+print(print_primes_between(number1,number2))
+
+      
+    
+      
