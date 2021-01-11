b4066bd6fa56b626ce7ad8129162b6ba79caf2d4
n34064730 <inandeniz.bal@gmail.com>
2021-01-11 14:05:09 +0000

    düzetlmeler

--- lab9/3.py
+++ lab9/3.py
@@ -1,15 +1,11 @@
 def get_evens(liste,counter=0):
   if len(liste)==0:
     return counter 
-
   if liste[0]%2==0:
-    counter+=1
-    print("hey")
-    return get_evens(liste[1:])
+    return get_evens(liste[1:],counter+1)
   elif liste[0]%2!=0:
-    liste.pop(0)
-    return get_evens(liste)
+    return get_evens(liste[1:],counter)
   
 
-x=get_evens([1,2,3,4])
+x=get_evens([1,2,3,4,8])
 print(x)
